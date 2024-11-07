45610437b3497b2f8e7e8e40474df501452b1eab
Tayler Hilton <Taylerch@byu.edu>
2024-11-07 11:53:35 -0700

    created view components

--- public/src/app.jsx
+++ public/src/app.jsx
@@ -6,10 +6,11 @@ export default function App() {
   return (
     <div className='app'>
         <header>
-            <h1 id = "title">Lingua Franca - Homepage</h1>
+            <h1 id = "title">Lingua Franca</h1>
 
+                {/* this will eventually only be displayed if we are past the login page */}
             <div class = "username_box">
-                <h3 id="username">User_Name789</h3>
+                <h3 id="username">User_Name789</h3> 
                 <img  id="user_profile_pic" src="Globe_icon.jpg" alt="User Profile Image"></img>
             </div>
         </header> 
@@ -19,7 +20,8 @@ export default function App() {
         <footer>
             <nav class = "nav_box">
                 <menu class = "link_menu">
-                <a href="menu.html">&lt;- Back</a>
+                    {/* this href will be replaced by a variable tracking what the previous page was at some point. */}
+                <a href="menu.html">&lt;- Back</a> 
                 <a href="login.html">Login</a>
                 <a href="menu.html">Home</a>
                 </menu>
---
+++ public/src/vocab/vocab.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Vocab() {
+  return (
+    <main className='vocab_box'>
+      <div>vocab displayed here</div>
+    </main>
+  );
+}
