3b2304e5223737135bfc43784ce9066e6d39eb89
Tayler Hilton <Taylerch@byu.edu>
2024-12-07 19:13:26 -0700

    uodated login

--- public/src/login/login.jsx
+++ public/src/login/login.jsx
@@ -1,83 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-export function Login() {
+import { Unauthenticated } from './unauthenticated';
+import { Authenticated } from './authenticated';
+import { AuthState } from './authState';
 
-  const [usernameChange, setUsernameChange] = useState(null);
-  const [passwordChange, setPasswordChange] = useState(null);
-  const [emailChange, setEmailChange] = useState(null);
-
-  const [quote, setQuote] = React.useState('Loading...');
-  const [quoteAuthor, setQuoteAuthor] = React.useState('unknown');
-
-  // We only want this to render the first time the component is created and so we provide an empty dependency list.
-  React.useEffect(() => {
-    fetch('https://quote.cs260.click')
-      .then((response) => response.json())
-      .then((data) => {
-        setQuote(data.quote);
-        setQuoteAuthor(data.author);
-      })
-      .catch();
-  }, []);
-
-  const handlePasswordChange = (e) => {
-    setPasswordChange(e.target.value);
-  };
-  const handleEmailChange = (e) => {
-    setEmailChange(e.target.value);
-  };
-  const handleUsernameChange = (e) => {
-    setUsernameChange(e.target.value);
-  };
-
-  async function handleClickLogin() {
-    loginOrCreate(`/api/auth/login`);
-  }
-
-  async function handleClickCreate() {
-    loginOrCreate(`/api/auth/create`);
-  }
-
-  async function loginOrCreate(endpoint) {
-    const response = await fetch(endpoint, {
-      method: 'post',
-      body: JSON.stringify({ username: usernameChange, email: emailChange, password: passwordChange }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    });
-    if (response?.status === 200) {
-      localStorage.setItem('userName', usernameChange);
-    } else {
-      const body = await response.json();
-      setDisplayError(`⚠ Error: ${body.msg}`);
-    }
-  }
-
-
+export function Login({ userName, authState, onAuthChange }) {
   return (
-    <div className = "body">
-      <main className='main_box'>
-        <form className = "index_input" method="get" action="vocab">
-          <div>
-              <span>Email: </span>
-              <input className = "simple_input" type="text" placeholder="your@email.com" onChange = {handleEmailChange}/>
-          </div>
-          <div>
-              <span>Username: </span>
-              <input className = "simple_input" type="text" placeholder="username" onChange = {handleUsernameChange}/>
-          </div>
-          <div>
-              <span>Password: </span>
-              <input className = "simple_input" type="password" placeholder="password" onChange={handlePasswordChange}/>
-          </div>
-          <div>
-              <button href = 'vocab' className = "simple_button login_button" onClick={() => handleClickLogin()}>Login</button>
-              <button href = "vocab" className = "simple_button login_button" onClick={() => handleClickCreate()}>Create</button>
-          </div>
-      </form>
-      <p className="quote">{quote} - {quoteAuthor}</p>
-      </main>
-    </div>
+    <main className='container-fluid bg-secondary text-center'>
+      <div>
+        {authState !== AuthState.Unknown && <h1>Welcome to Simon</h1>}
+        {authState === AuthState.Authenticated && (
+          <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
+        )}
+        {authState === AuthState.Unauthenticated && (
+          <Unauthenticated
+            userName={userName}
+            onLogin={(loginUserName) => {
+              onAuthChange(loginUserName, AuthState.Authenticated);
+            }}
+          />
+        )}
+      </div>
+    </main>
   );
 }
