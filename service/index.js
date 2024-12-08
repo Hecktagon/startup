5336c05519b401302897cf2896625d008b724604
Tayler Hilton <Taylerch@byu.edu>
2024-12-07 19:08:12 -0700

    started databas.js and updated index.js

--- service/index.js
+++ service/index.js
@@ -1,52 +1,78 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
 const express = require('express');
-const uuid = require('uuid');
 const app = express();
+const DB = require('./database.js');
+
+const authCookieName = 'token';
+
+// The service port may be set on the command line
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+// JSON body parsing using built-in middleware
 app.use(express.json());
-const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
-let figures = [];
-let users = {};
-let userProfilePic = '';
+// Use the cookie parser middleware for tracking authentication tokens
+app.use(cookieParser());
 
+// Serve up the applications static content
 app.use(express.static('public'));
 
-var apiRouter = express.Router();
+// Trust headers that are forwarded from the proxy so we can determine IP addresses
+app.set('trust proxy', true);
+
+// Router for service endpoints
+const apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
-
+// CreateAuth token for a new user
 apiRouter.post('/auth/create', async (req, res) => {
-    const user = users[req.body.username];
-    if (user) {
-      res.status(409).send({ msg: 'Existing user' });
-    } else {
-      const user = { email: req.body.email, password: req.body.password, token: uuid.v4() };
-      users[user.email] = user;
-  
-      res.send({ token: user.token });
-    }
-  });
+  if (await DB.getUser(req.body.email)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = await DB.createUser(req.body.email, req.body.password);
 
-  apiRouter.post('/auth/login', async (req, res) => {
-    const user = users[req.body.username];
-    if (user) {
-      if (req.body.password === user.password) {
-        user.token = uuid.v4();
-        res.send({ token: user.token });
-        return;
-      }
+    // Set the cookie
+    setAuthCookie(res, user.token);
+
+    res.send({
+      id: user._id,
+    });
+  }
+});
+
+// GetAuth token for the provided credentials
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await DB.getUser(req.body.email);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      setAuthCookie(res, user.token);
+      res.send({ id: user._id });
+      return;
     }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
+
+// DeleteAuth token if stored in cookie
+apiRouter.delete('/auth/logout', (_req, res) => {
+  res.clearCookie(authCookieName);
+  res.status(204).end();
+});
+
+// secureApiRouter verifies credentials for endpoints
+const secureApiRouter = express.Router();
+apiRouter.use(secureApiRouter);
+
+secureApiRouter.use(async (req, res, next) => {
+  const authToken = req.cookies[authCookieName];
+  const user = await DB.getUserByToken(authToken);
+  if (user) {
+    next();
+  } else {
     res.status(401).send({ msg: 'Unauthorized' });
-  });
-
-  apiRouter.delete('/auth/logout', (req, res) => {
-    const user = Object.values(users).find((u) => u.token === req.body.token);
-    if (user) {
-      delete user.token;
-    }
-    res.status(204).end();
-  });
-
- 
+  }
+});
 
   // fetchFigures
 apiRouter.get('/figures', (_req, res) => {
@@ -64,22 +90,35 @@ apiRouter.get('/figures', (_req, res) => {
     res.send(figures);
   });
 
-  app.use((_req, res) => {
-    res.sendFile('index.html', { root: 'public' });
-  });
-  
+  // Default error handler
+app.use(function (err, req, res, next) {
+  res.status(500).send({ type: err.name, message: err.message });
+});
 
-  app.listen(port, () => {
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+  res.sendFile('index.html', { root: 'public' });
+});
+
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
+
+const httpService = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
 
-
-  // updateFigures adds new figures to figures.
-  function updateFigures(newFigure, figures) {
-    figures.push(newFigure);
-    return figures
-  }
+// updateFigures adds new figures to figures.
+function updateFigures(newFigure, figures) {
+  figures.push(newFigure);
+  return figures
+}
 
   
 
