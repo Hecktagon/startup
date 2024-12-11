bc32f105908d94e2df675db77d6c0d6b8917c00a
Tayler Hilton <Taylerch@byu.edu>
2024-12-11 02:13:18 -0700

    Im confused

--- service/index.js
+++ service/index.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt');
 const express = require('express');
 const app = express();
 const DB = require('./database.js');
+const { peerProxy } = require('./peerProxy.js');
 
 const authCookieName = 'token';
 
@@ -124,6 +125,9 @@ function setAuthCookie(res, authToken) {
 const httpService = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+
+peerProxy(httpService);
 
 
 // updateFigures adds new figures to figures.
